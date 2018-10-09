595f9f7c9a7be5fc0c938663513947fb8bde665d
Apple PC <applepc@ApplePCs-MacBook-Pro.local>
2018-10-09 15:53:59 +0500

    inital comment

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,37 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient} = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',{ useNewUrlParser: true }, (err, client) =>{
+//console.log(err);
+  if(err) {
+  return console.log('Unable to connect Mongo Database Server');
+  }
+  console.log('Connected to Mongo Database Server');
+  const db = client.db('TodoApp');
+
+  
+
+  // db.collection('Todos').insertOne({
+  //   text: 'something to do',
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) {
+  //     console('Unable to Insert data', err);
+  //   } else {
+  //     console.log(JSON.stringify(result.ops, undefined, 2));
+  //   }
+  // });
+  // db.collection('Users').insertOne({
+  //   name: 'Shahbaz',
+  //   age: 30,
+  //   location: 'Lahore'
+  // }, (err, result) => {
+  //   if (err) {
+  //     console('Unable to Insert data', err);
+  //   } else {
+  //     console.log(result.ops[0]._id.getTimestamp());
+  //   }
+  // });
+  client.close();
+});
