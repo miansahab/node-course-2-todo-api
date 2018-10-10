88a6570c5b781b5ed21f0bc29d111f43b27c8a09
Apple PC <applepc@ApplePCs-MacBook-Pro.local>
2018-10-10 16:50:46 +0500

    Add get /todos route

--- server/server.js
+++ server/server.js
@@ -21,11 +21,21 @@ app.post('/todos', (req, res) => {
     });
 });
 
+app.get('/todos', (req, res) => {
 
+    Todo.find().then((todos) => {
+      res.send({todos});
+    });
+
+}, (e) => {
+    res.status(400).send(e);
+});
 
 app.listen(3000, () =>{
   console.log('app is listening on 3000');
 })
+
+module.exports = app;
 
 // var newTodo = new Todo({
 //   text: 'Cook dinner'
