49139eee7e02b5663b3887de33bf707478c4c36a
Apple PC <applepc@ApplePCs-MacBook-Pro.local>
2018-10-11 15:57:43 +0500

    Add Patch ./todos/:id

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,10 @@
 var mongoose = require('mongoose');
 
 mongoose.promise = global.promise;
+
+mongoose.set('useCreateIndex', true);       //https://stackoverflow.com/questions/51916630/mongodb-mongoose-collection-find-options-deprecation-warning
+mongoose.set('useFindAndModify', false);
+
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
