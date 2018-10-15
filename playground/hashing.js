1ce5989328599ba13aa101fa9c6afd2ab20a312c
Apple PC <applepc@ApplePCs-MacBook-Pro.local>
2018-10-15 14:16:32 +0500

    Add post /users/login route

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,29 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-}
-var token = jwt.sign(data, '123abc');
-console.log(token);
+var password = 'abc123!';
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded: ', decoded);
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt , (err, hash) => {
+    console.log(hash);
+  });
+});
+
+var hashedPassword = '$2a$10$mJAEsNYrjWXxR5zzBCeq0OlHWYHYw5OOHOJKEX6DPOKAlYeNfburW';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+// var data = {
+//   id: 10
+// }
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded: ', decoded);
 
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
